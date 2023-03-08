e5d693232aeedfe16a93c58bb8d7a37eb3a87a9f
Varun Verma <varunverma855@gmail.com>
2023-03-08 13:16:33 -0500

    Notion pipeline

--- utils/notion-test.py
+++ utils/notion-test.py
@@ -1,28 +1,64 @@
-from notion_client import Client
+import requests
 import os
-
 import sys
 sys.path.append('../') 
 from set_envs import setenvs
 
-
 setenvs()
-id = "script-6850ef2d6fdd446c88b07e4e00b4e48b"
+
+
+from notion_client import Client
 
 notion = Client(auth=os.environ['NOTION_TOKEN'])
 
-# Get the page containing the table
-page = notion.pages.retrieve(page_id=id)
+id = "babf47c2507d4d9ea27c78c83c797627"
+database_id = "48e417cfbba449efac29a20f836f8247"
 
-print(page)
+database = notion.databases.retrieve(database_id)
 
-# Get the table from the page
-# table = page.collection.get()
 
-# # Update a row in the table
-# row_to_update = table.get_rows()[0]  # Get the first row in the table
-# row_to_update.name = "New name"  # Update the name of the row
-# row_to_update.set_property("Status", "In progress")  # Update the value of a property
 
-# # Save the changes
-# row_to_update.save()
+new_row_properties = {
+ "Etherscan": {
+        "title": [
+            {
+                "text": {
+                    "content": "Some site"
+                }
+            }
+        ]
+    },
+    "Ether": {
+       "rich_text": [
+            {
+                "text": {
+                    "content": "USDT"
+                }
+            }
+        ]
+    },
+    "ERC20": {
+        "rich_text": [
+            {
+                "text": {
+                    "content": "USDT.com"
+                }
+            }
+        ]
+    },
+    "Notes": {
+        "rich_text": [
+            {
+                "text": {
+                    "content": "This is a test row"
+                }
+            }
+        ]
+    }
+}
+
+# Create new row in the database
+new_row = notion.pages.create(parent={"database_id": database_id}, properties=new_row_properties)
+
+# Print new row object
+print(new_row)
